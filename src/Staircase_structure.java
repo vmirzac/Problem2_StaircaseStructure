ca626d5e77e82514dcae2d916dc9b87b12d1bb0b
vmirzac <victormarzac@yahoo.com>
2019-05-27 13:59:49 +0300

    Initial commit

---
+++ src/Staircase_structure.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Staircase_structure {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+
+        int n = scan.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(j < n - 1 - i){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
